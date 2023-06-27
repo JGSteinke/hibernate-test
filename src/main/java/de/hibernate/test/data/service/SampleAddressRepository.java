7ebcd3bbe54d954bb3a204b719cbcfcb99eef4d1
start.vaadin.com <no-reply@vaadin.com>
2023-06-27 11:46:42 +0000

    Generated project

---
+++ src/main/java/de/hibernate/test/data/service/SampleAddressRepository.java
@@ -0,0 +1,12 @@
+package de.hibernate.test.data.service;
+
+import de.hibernate.test.data.entity.SampleAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SampleAddressRepository
+        extends
+            JpaRepository<SampleAddress, Long>,
+            JpaSpecificationExecutor<SampleAddress> {
+
+}
